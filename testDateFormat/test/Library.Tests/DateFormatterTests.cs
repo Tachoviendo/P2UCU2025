bf5bd6956594733b7d787cb20974ce647901b7ca
Tachoviendo <ignacio.3249@gmail.com>
2025-09-16 19:07:33 -0300

    create project

---
+++ testDateFormat/test/Library.Tests/DateFormatterTests.cs
@@ -0,0 +1,15 @@
+﻿namespace Library.Tests;
+
+public class DateFormatterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
